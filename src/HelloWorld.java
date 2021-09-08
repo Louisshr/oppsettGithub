486bd5498ac9375858376e1ab4e571f260e3902d
LouisVitenskap <70730534+LouisVitenskap@users.noreply.github.com>
2021-09-08 08:16:23 +0200

    Endret for-løkke. Løkken går fra 10 til 20 istedenfor 0 til 10

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
         System.out.println("Hello World!");
 
 
-        System.out.println("Petra teller til 10");
-        for (int i = 1; i <= 10; i++){
+        System.out.println("Petra teller fra 10 til 20");
+        for (int i = 10; i <= 20; i++){
             System.out.print(i + " ");
         }
     }
